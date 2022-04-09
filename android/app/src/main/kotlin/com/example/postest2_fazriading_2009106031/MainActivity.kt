73eb0db0791c55b6238723a7d64e65cd5c2831c9
fazrigading <super.fai700@gmail.com>
2022-04-10 02:45:34 +0800

    second commit

---
+++ android/app/src/main/kotlin/com/example/postest2_fazriading_2009106031/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.postest2_fazriading_2009106031
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:postest2_fazriading_2009106031/button.dart';
+import 'package:postest2_fazriading_2009106031/menu.dart';
+import 'package:postest2_fazriading_2009106031/nfts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Post-test 2 2009106031',
+      theme: ThemeData(fontFamily: 'Manrope'),
+      home: const Homepage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Image.asset('assets/ethcopy1.png',
+              fit: BoxFit.contain, height: 32, width: 32),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: const Text(
+              "12.89 ETH",
+              style: TextStyle(
+                  fontFamily: 'Manrope',
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+        ]),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(
+                  Icons.search,
+                  size: 26.0,
+                  color: Colors.black,
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.black,
+                ),
+              )),
+        ],
+        backgroundColor: Colors.white,
+        elevation: 1,
+        toolbarHeight: 70,
+      ),
+      body: ListView(
+        children: [
+          const Categories(),
+          const CategoriesButton(),
+          const FeaturedNFTs(),
+          SizedBox(
+              height: 460,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const <Widget>[
+                  NFT(
+                      kreator: 'Bored Ape Yacht Club',
+                      judul: 'Bored Ape #8854',
+                      harga: '25',
+                      gambar: 'assets/boredape8854.png'),
+                  NFT(
+                      kreator: 'MekaVerse',
+                      judul: 'Meka #3139',
+                      harga: '10',
+                      gambar: 'assets/Rectangle69.png'),
+                  NFT(
+                      kreator: 'Zeff Hood',
+                      judul: 'Dacing David',
+                      harga: '7',
+                      gambar: 'assets/image7.png'),
+                ],
+              )),
+          const FeaturedCreators(),
+          const Creators(),
+        ],
+      ),
+    );
+  }
+}
